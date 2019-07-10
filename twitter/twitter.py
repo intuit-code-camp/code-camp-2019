e39a6a5a738003e6ee3acb30206c42e2378e7759
Pillay, Parsana <Parsana_Pillay@intuit.com>
2019-07-10 16:52:04 -0600

    update twitter

--- twitter/twitter.py
+++ twitter/twitter.py
@@ -15,5 +15,14 @@ twitter = Twython(
 )
 
 message = "Hello world!"
+randomMessages = [
+    "Hello world",
+    "Hi there",
+    "What's up?",
+    "How's it going?",
+    "Have you been here before?",
+    "Get a hair cut!",
+]
+
 twitter.update_status(status=message)
 print("Tweeted: %s" % message)
